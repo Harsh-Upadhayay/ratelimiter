1302907aa3d6f1efabedabdbc8569b6b5bedb9a3
Harsh Upadhayay <harshupadhayay906@gmail.com>
2026-06-05 06:39:09 +0000

    test: add validation tests for Token Bucket capacity and refill rate

--- token_bucket_tests.go
+++ token_bucket_tests.go
@@ -1,1 +1,40 @@
 package ratelimiter
+
+import (
+	"testing"
+)
+
+func TestInvalidCapacityError(t *testing.T) {
+	_, err := NewTokenBucket(0, 5)
+
+	if err != ErrInvalidCapacity {
+		t.Fatalf("Bucket created with 0 capacity")
+	}
+	_, err = NewTokenBucket(-1, 5)
+
+	if err != ErrInvalidCapacity {
+		t.Fatalf("Bucket created with negative capacity")
+	}
+}
+
+func TestInvalidRefillRateError(t *testing.T) {
+	_, err := NewTokenBucket(5, 0)
+
+	if err != ErrInvalidRefillRate {
+		t.Fatalf("bucket created with 0 rate")
+	}
+	_, err = NewTokenBucket(4, -1)
+
+	if err != ErrInvalidRefillRate {
+		t.Fatalf("bucket created with negative refill rate")
+	}
+}
+
+// TODO: Add tests
+// new bucket starts full and consumes one token
+// rejects after capacity is exhausted
+// refills over elapsed time
+// does not allow with less than one token
+// caps refill at capacity
+// retry-after is based on missing tokens
+// backward time does not grant tokens
